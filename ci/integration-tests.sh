0893b5df1a0d93dced6e1568f8b261ecf21e7c59
Kai Mast <kaimast@cs.cornell.edu>
2018-12-02 23:52:04 -0500

    Fix working dir

--- ci/integration-tests.sh
+++ ci/integration-tests.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 
+export LIBRARY_PATH=/usr/lib:/usr/local/lib:$INSTALL_DIR/lib:$INSTALL_DIR/lib/x86_64-linux-gnu
+export LD_LIBRARY_PATH=/usr/lib:/usr/local/lib:$INSTALL_DIR/lib:$INSTALL_DIR/lib/x86_64-linux-gnu
+
 cd build
+
 ./churn-test listen 14444 &
 sleep 1
 ./churn-test connect localhost 14444 5 0
