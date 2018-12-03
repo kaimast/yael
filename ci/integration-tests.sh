5f6bbe3962d5a3bf2998388d3e0b2ac500fcb7a9
Kai Mast <kaimast@cs.cornell.edu>
2018-12-02 23:56:29 -0500

    Fix working dir

--- ci/integration-tests.sh
+++ ci/integration-tests.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+INSTALL_DIR=$HOME/local
+
 export LIBRARY_PATH=/usr/lib:/usr/local/lib:$INSTALL_DIR/lib:$INSTALL_DIR/lib/x86_64-linux-gnu
 export LD_LIBRARY_PATH=/usr/lib:/usr/local/lib:$INSTALL_DIR/lib:$INSTALL_DIR/lib/x86_64-linux-gnu
 
