5cda962825d469f88abeacc9ce114aac06a29e26
Kai Mast <kai@kaimast.com>
2024-10-11 18:17:19 -0700

    CI: Rework build

--- scripts/setup-yael.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-BUILDTYPE=release
-INSTALL_DIR=$HOME/local
-
-meson build -Dbuildtype=$BUILDTYPE --prefix=$INSTALL_DIR -Dbotan_dir=$INSTALL_DIR/include/botan-2
-cd build
-ninja -v
-ninja install -v
